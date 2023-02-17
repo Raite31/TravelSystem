868cfd64c692a36fb4aebcd8b137fbad99249e4d
Key <60214514+Raite31@users.noreply.github.com>
2023-02-17 14:25:21 +0800

    feat：路由调整

--- src/router/index.js
+++ src/router/index.js
@@ -14,37 +14,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ "@/views/login/index.vue"),
   },
 
-  // 主页
-  {
-    path: "/home",
-    name: "home",
-    component: () => import("@/views/travel/home/index.vue"),
-  },
-  {
-    path: "/strategy",
-    name: "strategy",
-    component: () => import("@/views/blog/strategy/Strategy.vue"),
-  },
-  {
-    path: "/diary",
-    name: "diary",
-    component: () => import("@/views/blog/diary/Diary.vue"),
-  },
-  {
-    path: "/destination",
-    name: "destination",
-    component: () => import("@/views/travel/destination/Destination.vue"),
-  },
-  {
-    path: "/diaryContent",
-    name: "diaryContent",
-    component: () => import("@/views/blog/diary/DiaryContent.vue"),
-  },
-  {
-    path: "/pushDiary",
-    name: "pushDiary",
-    component: () => import("@/views/blog/diary/PushDiary.vue"),
-  },
+  // 出游查询
   {
     path: "/statistics",
     name: "statistics",
@@ -74,6 +44,46 @@ const routes = [
           import("../views/statistics/epidemic/City_Epidemic.vue"),
       },
     ],
+  },
+
+  // 旅游的路由
+  // ----主页
+  {
+    path: "/home",
+    name: "home",
+    component: () => import("@/views/travel/home/index.vue"),
+  },
+  // ----目的地
+  {
+    path: "/destination",
+    name: "destination",
+    component: () => import("@/views/travel/destination/Destination.vue"),
+  },
+
+  // 博客的路由
+  // ----我也不知道是啥
+  {
+    path: "/strategy",
+    name: "strategy",
+    component: () => import("@/views/blog/strategy/Strategy.vue"),
+  },
+  // ----日记
+  {
+    path: "/diary",
+    name: "diary",
+    component: () => import("@/views/blog/diary/Diary.vue"),
+  },
+  // ----日记内容
+  {
+    path: "/diaryContent",
+    name: "diaryContent",
+    component: () => import("@/views/blog/diary/DiaryContent.vue"),
+  },
+  // ----写日记
+  {
+    path: "/pushDiary",
+    name: "pushDiary",
+    component: () => import("@/views/blog/diary/PushDiary.vue"),
   },
 ];
 
