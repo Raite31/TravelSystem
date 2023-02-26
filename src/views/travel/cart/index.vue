70673b4b0b5cbd1cf978b559c7ef7308d64c44da
Key <60214514+Raite31@users.noreply.github.com>
2023-02-26 11:06:32 +0800

    feat：购物车页面调整导航头

--- src/views/travel/cart/index.vue
+++ src/views/travel/cart/index.vue
@@ -280,17 +280,6 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-* {
-  margin: 0;
-  padding: 0;
-}
-
-html,
-body {
-  width: 100%;
-  height: 100%;
-  background: #f1f1f1;
-}
 
 .contain-all {
   width: 1200px;
